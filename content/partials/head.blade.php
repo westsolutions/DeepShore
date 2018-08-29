fa8d99c5732d51c1a80e71cc99bbefc59681fa02
sabrina <sabrina@codecomment.io>
2018-08-29 14:55:57 -0400

    sass

--- content/partials/head.blade.php
+++ content/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>{{ $site->global('site_title') }}</title>
-<link rel="stylesheet" href="{{ $site->asset('css/style.css') }}"/>
+<link rel="stylesheet" href="{{ $site->asset('css/main.css') }}"/>
 <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css"/>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet">
